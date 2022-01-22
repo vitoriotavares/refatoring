13ab7470c74841962e84117301bab0582c867daf
Vitorio <norbdus@gmail.com>
2022-01-22 19:35:58 -0300

    getting customer and adding to intermediary object

--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
 function statement(invoice, plays) {
 	const statementData = {};
-	
+
 	statementData.customer = invoice.customer;
 	statementData.performances = invoice.performances;
 
 	return renderPlainText(statementData, plays);
+
+	function renderPlainText(data, plays) {
+		let result = `Statement for ${data.customer}\n`
+
+		for(let perf of data.performances){
+			result += `\n${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience} seats)`;
+		}
+
+		result += `\nAmount owed is ${totalAmount()}\n`;
+		result += `You earned ${totalVolumeCredits()} credits\n`;
+		return result;
+
+		function totalAmount() {
+			let result = 0;
+			for(let perf of data.performances){
+				result += amountFor(perf);
+			}
+			return usd(result);
+		}
+
+		function totalVolumeCredits() {
+			let result = 0;
+			for(let perf of data.performances){
+				result += volumeCreditsFor(perf);
+			}
+			return result;
+		}
+	}
 	
 	function amountFor(aPerformance) {
 		let result = 0;
@@ -47,36 +75,7 @@ function statement(invoice, plays) {
 			currency: "USD", 
 			minumumFractionDigits: 2
 		}).format(aNumber/100);
-	}
-
-	function totalVolumeCredits() {
-		let result = 0;
-		for(let perf of invoice.performances){
-			result += volumeCreditsFor(perf);
-		}
-		return result;
-	}
-
-	function totalAmount() {
-		let result = 0;
-		for(let perf of invoice.performances){
-			result += amountFor(perf);
-		}
-		return usd(result);
-	}
-	
-	function renderPlainText(data, plays) {
-		let result = `Statement for ${data.customer}\n`
-
-		for(let perf of data.performances){
-			result += `\n${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience} seats)`;
-		}
-
-		result += `\nAmount owed is ${totalAmount()}\n`;
-		result += `You earned ${totalVolumeCredits()} credits\n`;
-		return result;
-	}
-		
+	}	
 }
 
 
