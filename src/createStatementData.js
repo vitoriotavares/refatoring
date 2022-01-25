3e8e02f850caed2669d797423e90f81a23d1487d
Vitorio <norbdus@gmail.com>
2022-01-24 21:05:03 -0300

    separiting into files

---
+++ src/createStatementData.js
@@ -0,0 +1,62 @@
+function createStatementData(invoice, plays){
+    const statementData = {};
+
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+
+    function totalAmount(data) {
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+    }
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
+
+        function playFor(aPerformance) {
+            return plays[aPerformance.playID]
+        }
+
+        function amountFor(aPerformance) {
+            let result = 0;
+        
+            switch(aPerformance.play.type) {
+                case "tragedy":
+                    result = 40000;
+                    if(aPerformance.audience > 30){
+                        result += 1000*(aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if(aPerformance.audience > 20){
+                        result += 10000 + 500*(aPerformance.audience - 20)
+                    }
+                    result += 300*aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`unknown type: ${aPerformance.play.type}`);
+            }
+            return result;
+        }
+
+        function volumeCreditsFor(aPerfomance) {
+            let result = 0;
+            result += Math.max(aPerfomance.audience - 30,0);
+            if ("comedy" === aPerfomance.play.type) 
+                result += Math.floor(aPerfomance.audience / 5)
+            return result;
+        }
+    }	
+}
+
+module.exports = { createStatementData }
---
+++ src/statement.js
@@ -0,0 +1,1 @@
+import { createStatementData } from "./createStatementData";
