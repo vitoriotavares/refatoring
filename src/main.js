a3fdb96f98e3220496ccdf0024429217ce49afae
Vitorio <norbdus@gmail.com>
2022-01-24 21:32:28 -0300

    adding html output from statement

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 let { createStatementData } = require("./createStatementData");
 
 function statement(invoice, plays) {
-	
-	return renderPlainText(createStatementData(invoice, plays));
 
-	function renderPlainText(data, plays) {
+	return plainTextStatement(invoice, plays)
+	
+	function plainTextStatement(invoice, plays){
+		return renderPlainText(createStatementData(invoice, plays));
+	}
+
+	function htmlStatement(invoice, plays){
+		return renderHtml(createStatementData(invoice, plays));
+	}
+
+	function renderHtml(data){
+		let result = `<h1>Statement for ${data.customer}</h1>\n`
+		result += '<table>\n'
+		result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>'
+
+        for(let perf of data.performances){
+            result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}</td><td>${usd(perf.amount)}</td></tr>`;
+        }
+		result += '</table>\n'
+        result += `<p>Amount owed is ${data.totalAmount}</p>\n`;
+        result += `<p>You earned ${data.totalVolumeCredits} credits</p>\n`;
+        return result;
+	}
+
+	function renderPlainText(data) {
         let result = `Statement for ${data.customer}\n`
 
         for(let perf of data.performances){
